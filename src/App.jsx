7e872611d1f5d8a96567754b7824db65e555f018
Transpose <tofexzyno@gmail.com>
2025-05-31 18:12:11 +0100

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,10 @@ import LightDark from "./Components/LightDark";
 const App = () => {
   return (
     <div id="app" className="py-2 min-w-80 bg-black w-screen box-border">
-<<<<<<< HEAD
       <div className="flex w-screen justify-around items-center">
         <Navbar />
         <LightDark />
       </div>
-=======
-      <Navbar />
->>>>>>> f7cfc140147752df6cab344e663cccdd72950014
       <Hero />
       <Skills />
       <Projects />
