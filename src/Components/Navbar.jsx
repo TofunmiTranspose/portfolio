eab3b02343803ed07804ff0276f452f969d8b420
Transpose <tofexzyno@gmail.com>
2025-05-31 18:51:48 +0100

    continue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,20 @@
-import React from 'react';
+import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 const Navbar = () => {
   return (
-<<<<<<< HEAD
     <div className="flex justify-between w-9/10 items-center">
       <div className="bg-gray-800 w-7/10 h-15 px-1 py-3 items-center justify-around rounded-full flex">
-=======
-    <div className="mx-auto flex justify-between w-9/10">
-      <div className=" w-7/10 h-20 px-1 py-3 items-center justify-around rounded-full flex">
->>>>>>> f7cfc140147752df6cab344e663cccdd72950014
         <h2 className="text-xl font-bold text-white">TRANSPOSE</h2>
         <button className="bg-teal-500 px-3  py-1 rounded-full text-white font-semibold text-sm">
           <a href="https://wa.link/yjileb">Hire Me</a>
         </button>
       </div>
-        <div className="flex items-center justify-center h-10 w-10">
-          <MenuIcon
-            className="text-white rounded-full font-sm m-0"
-            style={{ fontSize: "25px", Padding: "5px" }}
-          />
-        </div>
-<<<<<<< HEAD
-=======
+      <div className="flex items-center justify-center h-10 w-10">
+        <MenuIcon
+          className="text-white rounded-full font-sm m-0"
+          style={{ fontSize: "25px", Padding: "5px" }}
+        />
       </div>
-        <FaSun className="text-white text-md"/>
->>>>>>> f7cfc140147752df6cab344e663cccdd72950014
     </div>
   );
 };
